9928783756d29842162a087dfb69bfd123958428
havetkd <58536445+havetkd@users.noreply.github.com>
2022-03-23 19:40:49 +0900

    Update HelloController.java

--- dev/doit1/src/main/java/com/itistory/doit1/Controller/HelloController.java
+++ dev/doit1/src/main/java/com/itistory/doit1/Controller/HelloController.java
@@ -26,7 +26,7 @@ public class HelloController {
     public String give(){
         return "gite page";
     }
-    @GetMapping("/give22")
+    @GetMapping("/give2343434342")
     public String give22(){
         return "gite22 page";
     }
